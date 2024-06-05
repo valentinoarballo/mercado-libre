97eb8730cfd66b243498e27178016c0bebc830c3
Nazareno Bucciarelli <bnazareno03@gmail.com>
2024-06-05 20:38:58 -0300

    first commit for branch

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import './App.css'
+import Navbar from './componets/Navbar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <Navbar></Navbar>
       <div className='bg-red-500'>
         aaaaaaaa
       </div>
