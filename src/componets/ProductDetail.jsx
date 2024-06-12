c7e0b7ee9d613aaa2119b18ac2e4eef82488c2df
Nazareno Bucciarelli <bnazareno03@gmail.com>
2024-06-11 21:04:18 -0300

    add description to product detail

--- src/componets/ProductDetail.jsx
+++ src/componets/ProductDetail.jsx
@@ -1,18 +1,54 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 const ProductDetail = (props) => {
+    const [productDescription, setProductDescription] = useState();
 
-    return(
-        <section className="container bg-white p-20">
-            <div className="flex">
-                    <div className="w-2/3 flex justify-center border-b border-b-gray-300 p-5">
-                            <img src={props.item.pictures[0].url} />
+    useEffect(() => {
+        getDescription();
+    }, [])
+
+    const getDescription = async () => {
+        try {
+            const descriptionResponse = await axios.get(`https://api.mercadolibre.com/items/${props.item.id}/description`,
+                {
+                    headers: {
+                        'Content-Type': 'application/json'
+                    }
+                }
+            )
+            const data = descriptionResponse.data;
+            setProductDescription(data.plain_text);
+        } catch (error) {
+            console.error("An error has occurred while fetching item's description", error);
+        }
+    }
+
+    return (
+        <section className="container bg-white flex justify-center">
+            <div className="w-9/12 ">
+                <div className="flex p-20">
+                    <div className="w-2/3 flex justify-center border-b border-b-gray-300 p-5 m-4">
+                        <img src={props.item.pictures[0].url} />
                     </div>
-                    <div className="w-1/3 flex flex-col justify-start items-start text-black">
-                        {props.item.condition === "new" && <p className="text-gray-400 text-sm">Nuevo</p>}
-                        <h1 className="text-start text-2xl font-semibold">{props.item.title}</h1>
-                        <p className="line-through">{props.item.base_price > props.item.price && props.item.base_price}</p>
-                        <h1 className="font-thin">$ {props.item.price}</h1>
+                    <div className="w-1/3 flex flex-col items-start text-black border rounded-lg border-gray-300 p-5">
+                        <div>
+                            {props.item.condition === "new" && <p className="text-gray-400 text-sm">Nuevo</p>}
+                            <h1 className="text-start text-1xl font-medium my-2">{props.item.title}</h1>
+                            <p className="line-through">{props.item.base_price > props.item.price && props.item.base_price}</p>
+                            <h1 className="text-3xl font-light">$ {props.item.price}</h1>
+                        </div>
+                        <div>
+
+                        </div>
+
                     </div>
+                </div>
+                <div className="container">
+                    <p>
+                        {productDescription && productDescription}
+                    </p>
+                </div>
             </div>
         </section>
     )
