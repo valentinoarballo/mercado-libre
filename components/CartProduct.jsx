74ec44c418bb50f562e3cffa55d775edab400878
Nazareno Bucciarelli <bnazareno03@gmail.com>
2024-06-18 21:51:39 -0300

    fix conditional rendering and change name of amount property to quantity

--- components/CartProduct.jsx
+++ components/CartProduct.jsx
@@ -4,7 +4,7 @@ import { useEffect, useState } from "react";
 
 const CartProduct = (props) => {
   const [product, setProduct] = useState();
-  const [amount, setAmount] = useState(props.amount);
+  const [quantity, setQuantity] = useState(props.quantity);
   const [hidden, setHidden] = useState(false);
   const [sellerLink, setSellerLink] = useState();
 
@@ -17,7 +17,7 @@ const CartProduct = (props) => {
       const productResponse = await axios.get(`https://api.mercadolibre.com/items/${props.id}`);
       const data = productResponse.data
       setProduct(data);
-      props.addPrice(data.price * amount);
+      props.addPrice(data.price * quantity);
       setSellerLink("https://listado.mercadolibre.com.ar/_CustId_" + data.seller_id);
     } catch (error) {
       console.error(error);
@@ -26,23 +26,23 @@ const CartProduct = (props) => {
 
   const remove = () => {
     setHidden(true);
-    props.addPrice(parseInt("-" + (product.price * amount)));
+    props.addPrice(parseInt("-" + (product.price * quantity)));
     const cartProductsInfoLocalStorage = JSON.parse(localStorage.getItem("cartProducts"));
     const cartProductsInfoUpdated = cartProductsInfoLocalStorage.filter((prod) => prod.id !== product.id);
     localStorage.setItem("cartProducts", JSON.stringify(cartProductsInfoUpdated));
   }
 
 
-  const increaseDecreaseAmount = (increase) => {
-    if (!increase && amount === 1) {
+  const increaseDecreaseQuantity = (increase) => {
+    if (!increase && quantity === 1) {
       return;
     }
-    setAmount(amount + (increase ? 1 : -1));
+    setQuantity(quantity + (increase ? 1 : -1));
     props.addPrice(increase ? product.price : parseInt("-" + product.price));
     const cartProductsInfoLocalStorage = JSON.parse(localStorage.getItem("cartProducts"));
     const cartProductsUpdated = cartProductsInfoLocalStorage.map((prod) => {
       if (prod.id === product.id) {
-        prod.amount += (increase ? 1 : -1);
+        prod.quantity += (increase ? 1 : -1);
       } return prod;
     })
     localStorage.setItem("cartProducts", JSON.stringify(cartProductsUpdated));
@@ -67,14 +67,14 @@ const CartProduct = (props) => {
           </div>
         </div>
         <div className="flex items-center max-w-[8rem] border border-gray-200">
-          <button type="button" id="decrement-button" data-input-counter-decrement="quantity-input" onClick={() => { increaseDecreaseAmount(false) }} className="hover:bg-gray-200 p-3 h-11 focus:ring-gray-100 focus:ring-2 focus:outline-none">
+          <button type="button" id="decrement-button" data-input-counter-decrement="quantity-input" onClick={() => { increaseDecreaseQuantity(false) }} className="hover:bg-gray-200 p-3 h-11 focus:ring-gray-100 focus:ring-2 focus:outline-none">
             <svg className="w-3 h-3 text-gray-900" aria-hidden="true" fill="none" viewBox="0 0 18 2">
               <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 1h16" />
             </svg>
           </button>
           <input type="text" id="quantity-input" data-input-counter data-input-counter-min="1" data-input-counter-max="50" aria-describedby="helper-text-explanation" className="border-x-0 border-gray-300 h-11 text-center text-gray-900 text-sm focus:ring-blue-500 focus:border-blue-500 block w-full py-2.5"
-            disabled value={amount} required />
-          <button type="button" id="increment-button" data-input-counter-increment="quantity-input" onClick={() => { increaseDecreaseAmount(true) }} className="hover:bg-gray-200 -e-lg p-3 h-11 focus:ring-gray-100 focus:ring-2 focus:outline-none">
+            disabled value={quantity} required />
+          <button type="button" id="increment-button" data-input-counter-increment="quantity-input" onClick={() => { increaseDecreaseQuantity(true) }} className="hover:bg-gray-200 -e-lg p-3 h-11 focus:ring-gray-100 focus:ring-2 focus:outline-none">
             <svg className="w-3 h-3 text-gray-900" aria-hidden="true" fill="none" viewBox="0 0 18 18">
               <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 1v16M1 9h16" />
             </svg>
--- components/CartSummary.jsx
+++ components/CartSummary.jsx
@@ -22,11 +22,11 @@ function CartSummary() {
           <div className="border-b border-b-gray-200">
             <button className="text-center p-4 w-1/6 text-blue-600 font-medium border-b-2 border-b-blue-600">Carrito </button>
           </div>
-          {cartProductsInfo && (cartProductsInfo.length > 0 ? cartProductsInfo.map((cartProduct, index) => {
-            return <CartProduct key={index} id={cartProduct.id} addPrice={addPrice} amount={cartProduct.amount}></CartProduct>;
-          }) : <h2 className="text-center pt-20 text-gray-800 font-medium text-2xl">No tienes productos en el carrito aún, ¡Sigue navegando!</h2>)
+          {cartProductsInfo && cartProductsInfo.length > 0 ? cartProductsInfo.map((cartProduct, index) => {
+            return <CartProduct key={index} id={cartProduct.id} addPrice={addPrice} quantity={cartProduct.quantity}></CartProduct>;
+          }) : <h2 className="text-center pt-20 text-gray-800 font-medium text-2xl">No tienes productos en el carrito aún, ¡Sigue navegando!</h2>
           }
-          <div className={cartProductsInfo && (cartProductsInfo.length === 0 ? "hidden" : "flex items-center pt-10 gap-10 text-2xl justify-end")}>
+          <div className={cartProductsInfo && cartProductsInfo.length === 0 ? "hidden" : "flex items-center pt-10 gap-10 text-2xl justify-end"}>
             <p>Total:</p>
             <p>$ {total.toLocaleString("es-AR")}</p>
           </div>
