79bbf27182538edadaf32f9433a05d605645a709
valentino <v.arballo@itecriocuarto.org.ar>
2024-06-24 19:31:08 -0300

    general fix

--- components/CardLoading.jsx
+++ components/CardLoading.jsx
@@ -3,17 +3,17 @@ import React from 'react'
 function CardLoading() {
   return (
     <article className='p-5'>
-      <div class="bg-white rounded-lg px-4 pb-4 flex flex-col justify-evenly h-auto min-h-[375px] xl:w-[19rem] lg:w-[25rem] w-[30rem] cursor-wait shadow-lg hover:shadow-xl">
-        <div class="rounded-lg animate-pulse bg-gray-300 h-[13rem] w-auto"></div>
-        <div class="animate-pulse flex space-x-4">
-          <div class="flex-1 space-y-6 py-1">
-            <div class="h-2 bg-gray-300 rounded"></div>
-            <div class="space-y-3">
-              <div class="grid grid-cols-3 gap-4">
-                <div class="h-2 bg-gray-300 rounded col-span-2"></div>
-                <div class="h-2 bg-gray-300 rounded col-span-1"></div>
+      <div className="bg-white rounded-lg px-4 pb-4 flex flex-col justify-evenly h-auto min-h-[375px] xl:w-[19rem] lg:w-[25rem] w-[30rem] cursor-wait shadow-lg hover:shadow-xl">
+        <div className="rounded-lg animate-pulse bg-gray-300 h-[13rem] w-auto"></div>
+        <div className="animate-pulse flex space-x-4">
+          <div className="flex-1 space-y-6 py-1">
+            <div className="h-2 bg-gray-300 rounded"></div>
+            <div className="space-y-3">
+              <div className="grid grid-cols-3 gap-4">
+                <div className="h-2 bg-gray-300 rounded col-span-2"></div>
+                <div className="h-2 bg-gray-300 rounded col-span-1"></div>
               </div>
-              <div class="h-2 bg-gray-300 rounded"></div>
+              <div className="h-2 bg-gray-300 rounded"></div>
             </div>
           </div>
         </div>
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -14,9 +14,9 @@ const Navbar = () => {
           <img src="/images/logo.png" className="h-8" alt="Mercado Libre Logo" />
         </a>
         <div>
-          <div className="relative hidden md:block">
+          <div className="relative hidden md:flex">
             <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
-              <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" fill="none" viewBox="0 0 20 20">
+              <svg className="w-4 h-4 text-gray-500" aria-hidden="true" fill="none" viewBox="0 0 20 20">
                 <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
               </svg>
               <span className="sr-only">Search icon</span>
@@ -27,7 +27,7 @@ const Navbar = () => {
               value={searchInput}
               onChange={handleSearch}
               onKeyUp={handleKeyUp}
-              className="block w-full p-2 px-10 ps-10 pr-40 text-lg text-gray-900 border border-gray-400 bg-gray-50 focus:ring-blue-500 focus:border-blue-500 " placeholder="Buscar productos, marcas y más..."
+              className="block w-full p-2 px-10 ps-10 pr-40 text-lg text-gray-900 border border-gray-400 bg-gray-50 focus:ring-blue-500 focus:border-blue-500 " placeholder="Buscar productos y más..."
             />
           </div>
           <button type="button" data-collapse-toggle="navbar-search" aria-controls="navbar-search" aria-expanded="false" className="md:hidden transition hover:transition text-gray-600 hover:text-gray-500 p-2.5 me-1">
@@ -53,13 +53,13 @@ const Navbar = () => {
         <div className={navbarMenuHidden ? 'hidden' : "items-center justify-between w-full md:flex md:w-auto md:order-1"} id="navbar-search">
           <div className="relative mt-3 md:hidden">
             <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
-              <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" fill="none" viewBox="0 0 20 20">
+              <svg className="w-4 h-4 text-gray-500" aria-hidden="true" fill="none" viewBox="0 0 20 20">
                 <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
               </svg>
             </div>
             <input
               type="text"
-              id="searchBar"
+              id="searchBarMobile"
               value={searchInput}
               onChange={handleSearch}
               onKeyUp={handleKeyUp}
